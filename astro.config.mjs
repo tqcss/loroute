308d6c535bae2331499cba91760bb21a46e62ac4
tqcss <134121905+tqcss@users.noreply.github.com>
2026-06-15 16:53:27 +0800

    chore: format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  srcDir: './src/client',
+  srcDir: "./src/client",
 
   vite: {
     plugins: [tailwindcss()],
